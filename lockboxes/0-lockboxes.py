249de9268d9e05046a2ddf935609fac867998179
GoldenHatchet15 <raphsant20@gmail.com>
2024-09-04 12:18:00 -0400

    lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module contains a function `canUnlockAll` that determines
+if all boxes can be unlocked in a set of boxes where each box
+may contain keys to other boxes.
+"""
 def canUnlockAll(boxes):
     n = len(boxes)
     unlocked = [False] * n
