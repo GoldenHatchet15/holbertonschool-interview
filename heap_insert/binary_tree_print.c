7591d13edfe8cddb6ab12b43ef97707256b2b44c
GoldenHatchet15 <raphsant20@gmail.com>
2024-09-15 20:32:04 -0400

    new node

--- heap_insert/binary_tree_print.c
+++ heap_insert/binary_tree_print.c
@@ -1,109 +1,99 @@
-#include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> /* Include string.h for memset and strlen */
+#include <string.h>
+#include "binary_trees.h"
 
-/* Utility function prototypes */
-static size_t _height(const binary_tree_t *tree);
-static size_t _print_t(const binary_tree_t *tree, int is_left, int offset, int depth, char **s);
+/* Original code from http://stackoverflow.com/a/13755911/5184480 */
 
 /**
- * binary_tree_print - Prints a binary tree
- * @tree: Pointer to the root node of the tree to print
+ * print_t - Stores recursively each level in an array of strings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
  */
-void binary_tree_print(const binary_tree_t *tree)
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 {
-    char **s;
-    size_t height, i, j;
+	char b[6];
+	int width, left, right, is_left, i;
 
-    if (!tree)
-        return;
-
-    height = _height(tree);
-    s = malloc(sizeof(*s) * (height * 2));
-    if (!s)
-        return;
-
-    for (i = 0; i < height * 2; i++)
-    {
-        s[i] = malloc(255);
-        if (!s[i])
-            return;
-        memset(s[i], 32, 255); /* Fill the string with spaces */
-    }
-
-    _print_t(tree, 0, 0, 0, s);
-
-    for (i = 0; i < height * 2; i++)
-    {
-        for (j = 254; j > 1 && s[i][j] == ' '; j--)
-            ;
-        s[i][j + 1] = '\0';
-        printf("%s\n", s[i]);
-        free(s[i]);
-    }
-    free(s);
+	if (!tree)
+		return (0);
+	is_left = (tree->parent && tree->parent->left == tree);
+	width = sprintf(b, "(%03d)", tree->n);
+	left = print_t(tree->left, offset, depth + 1, s);
+	right = print_t(tree->right, offset + left + width, depth + 1, s);
+	for (i = 0; i < width; i++)
+		s[depth][offset + left + i] = b[i];
+	if (depth && is_left)
+	{
+		for (i = 0; i < width + right; i++)
+			s[depth - 1][offset + left + width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	else if (depth && !is_left)
+	{
+		for (i = 0; i < left + width; i++)
+			s[depth - 1][offset - width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	return (left + width + right);
 }
 
 /**
  * _height - Measures the height of a binary tree
- * @tree: Pointer to the node to measure the height
  *
- * Return: The height of the tree
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
  */
 static size_t _height(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t height_l;
+	size_t height_r;
 
-    if (!tree)
-        return (0);
-    left = tree->left ? 1 + _height(tree->left) : 0;
-    right = tree->right ? 1 + _height(tree->right) : 0;
-    return (left > right ? left : right);
+	height_l = tree->left ? 1 + _height(tree->left) : 0;
+	height_r = tree->right ? 1 + _height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
 
 /**
- * _print_t - Stores recursively each level of the binary tree into a string
- * @tree: Pointer to the node to print
- * @is_left: Flag indicating whether the node is a left child
- * @offset: Offset where the root is printed
- * @depth: Depth of the node
- * @s: Buffer to store the print
+ * binary_tree_print - Prints a binary tree
  *
- * Return: Number of characters printed
+ * @tree: Pointer to the root node of the tree to print
  */
-static size_t _print_t(const binary_tree_t *tree, int is_left, int offset, int depth, char **s)
+void binary_tree_print(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	char **s;
+	size_t height, i, j;
 
-    char b[6];
-    size_t width, left, right;
-
-    /* Add debug print */
-    printf("Processing node with value: %d, depth: %d, offset: %d\n", tree->n, depth, offset);
-
-    sprintf(b, "(%03d)", tree->n);
-    width = strlen(b);
-
-    left = _print_t(tree->left, 1, offset, depth + 1, s);
-    right = _print_t(tree->right, 0, offset + left + width, depth + 1, s);
-
-    for (size_t i = 0; i < width; i++)
-        s[depth * 2][offset + left + i] = b[i];
-
-    if (depth && is_left)
-    {
-        for (size_t i = 0; i < width + right; i++)
-            s[(depth * 2) - 1][offset + left + width / 2 + i] = '-';
-        s[(depth * 2) - 1][offset + left + width / 2] = '.';
-    }
-    else if (depth && !is_left)
-    {
-        for (size_t i = 0; i < left + width; i++)
-            s[(depth * 2) - 1][offset - width / 2 + i] = '-';
-        s[(depth * 2) - 1][offset + left + width / 2] = '.';
-    }
-
-    return (left + width + right);
+	if (!tree)
+		return;
+	height = _height(tree);
+	s = malloc(sizeof(*s) * (height + 1));
+	if (!s)
+		return;
+	for (i = 0; i < height + 1; i++)
+	{
+		s[i] = malloc(sizeof(**s) * 255);
+		if (!s[i])
+			return;
+		memset(s[i], 32, 255);
+	}
+	print_t(tree, 0, 0, s);
+	for (i = 0; i < height + 1; i++)
+	{
+		for (j = 254; j > 1; --j)
+		{
+			if (s[i][j] != ' ')
+				break;
+			s[i][j] = '\0';
+		}
+		printf("%s\n", s[i]);
+		free(s[i]);
+	}
+	free(s);
 }
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,23 +1,25 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 
-/* Binary Tree structure */
-struct binary_tree_s
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s heap_t;
-
-/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree); /* Add this prototype */
+void binary_tree_print(const binary_tree_t *);
 
-#endif /* BINARY_TREES_H */
-
+#endif /* _BINARY_TREES_H_ */
