c4d5f6d69489ef9702eacf17e4cb53cfe7b5b224
GoldenHatchet15 <raphsant20@gmail.com>
2024-09-15 21:17:44 -0400

    Heap - Insert

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,104 +1,88 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
- * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree
- *
- * Return: Size of the tree
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
  */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (!tree)
+	size_t size = 0, r = 0, l = 0;
+
+	if (tree == NULL)
+	{
 		return (0);
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
-}
-
-/**
- * heapify_up - Maintains the max heap property by swapping nodes
- * @node: Pointer to the newly inserted node
- *
- * Return: Pointer to the inserted node after heapifying
- */
-heap_t *heapify_up(heap_t *node)
-{
-	int temp;
-
-	while (node->parent && node->n > node->parent->n)
-	{
-		/* Swap values with the parent */
-		temp = node->n;
-		node->n = node->parent->n;
-		node->parent->n = temp;
-		node = node->parent;
 	}
-	return (node);
-}
-
-/**
- * insert_node - Helper function to insert a
- * node at the next available position
- * @root: Pointer to the root of the heap
- * @value: Value to insert
- *
- * Return: Pointer to the inserted node, or NULL on failure
- */
-heap_t *insert_node(heap_t *root, int value)
-{
-	size_t size;
-	size_t index;
-	heap_t *new_node, *parent;
-	int parent_dir;
-
-	size = binary_tree_size(root);
-	index = size + 1;  /* Position of the new node in a complete binary tree */
-	new_node = binary_tree_node(NULL, value);
-	if (!new_node)
-		return (NULL);
-
-	parent = root;
-	for (parent_dir = index / 2; parent_dir > 1; parent_dir /= 2)
-	{
-		if (parent_dir % 2 == 0)
-			parent = parent->left;
-		else
-			parent = parent->right;
-	}
-
-	/* Insert as left or right child */
-	if (index % 2 == 0)
-		parent->left = new_node;
 	else
-		parent->right = new_node;
-	new_node->parent = parent;
-
-	return (new_node);
+	{
+		l = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		size = r + l + 1;
+	}
+	return (size);
 }
 
 /**
- * heap_insert - Inserts a value into a Max Binary Heap
- * @root: Double pointer to the root node of the Heap
- * @value: Value to store in the node to be inserted
- *
- * Return: Pointer to the inserted node, or NULL on failure
+ * insert_ordered - Inserts node level ordered
+ * @tree: Type binary_tree pointer of the parent node
+ * @value: Type int value of value inserted into node
+ * @idx: Type int index of the node
+ * @n_leaf: Type int node array position (parent formula: idx / 2)
+ * Return: Node inserted
+ */
+heap_t *insert_ordered(binary_tree_t *tree, int value, int idx, int n_leaf)
+{
+	heap_t *node;
+
+	if (tree == NULL)
+	{
+		return (NULL);
+	}
+	if (idx + 1 == n_leaf / 2)
+	{
+		if (n_leaf & 1)
+		{
+			tree->right = binary_tree_node(tree, value);
+			return (tree->right);
+		}
+		else
+		{
+			tree->left = binary_tree_node(tree, value);
+			return (tree->left);
+		}
+	}
+	node = insert_ordered(tree->left, value, 2 * idx + 1, n_leaf);
+	if (node != 0)
+	{
+		return (node);
+	}
+	else
+	{
+		return (insert_ordered(tree->right, value, 2 * idx + 2, n_leaf));
+	}
+}
+/**
+ * heap_insert - Insert a node into a Max binary heap
+ * @root: Type heap_t pointer of the parent node
+ * @value: Type int of value inserted into node
+ * Return: Node inserted
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node;
+	heap_t *node;
 
-	if (!root)
-		return (NULL);
-
-	if (*root == NULL)  /* If the tree is empty, create the root node */
+	if (*root == NULL)
 	{
 		*root = binary_tree_node(NULL, value);
 		return (*root);
 	}
-
-	new_node = insert_node(*root, value);
-	if (!new_node)
-		return (NULL);
-
-	return (heapify_up(new_node));
-    /* Heapify tree to maintain max-heap property */
+	node = insert_ordered(*root, value, 0, binary_tree_size(*root) + 1);
+	while (node->parent && node->n > node->parent->n)
+	{
+		node->n = node->parent->n;
+		node->parent->n = value;
+		node = node->parent;
+	}
+	return (node);
 }
