f329003345150708d308a39499fc2eeea39c109a
GoldenHatchet15 <raphsant20@gmail.com>
2024-10-07 18:15:57 -0400

    Log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Reads from stdin and computes metrics."""
 import sys
 
 total_file_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
+
 
 def print_stats():
     """Print the metrics."""
@@ -11,6 +13,7 @@ def print_stats():
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{}: {}".format(code, status_codes[code]))
+
 
 try:
     for line in sys.stdin:
