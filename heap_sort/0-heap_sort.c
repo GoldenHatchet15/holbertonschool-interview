2b2de169552bf0681636baf191c2b73bb94eda88
GoldenHatchet15 <raphsant20@gmail.com>
2025-02-17 16:30:10 -0400

    heap_sort

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stddef.h>
 
 /**
  * swap - Swaps two integers in an array.
@@ -7,9 +8,9 @@
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -21,22 +22,22 @@ void swap(int *a, int *b)
  */
 void heapify(int *array, size_t size, size_t n, size_t i)
 {
-    size_t largest = i;
-    size_t left = 2 * i + 1;
-    size_t right = 2 * i + 2;
+	size_t largest = i;
+	size_t left = 2 * i + 1;
+	size_t right = 2 * i + 2;
 
-    if (left < n && array[left] > array[largest])
-        largest = left;
+	if (left < n && array[left] > array[largest])
+		largest = left;
 
-    if (right < n && array[right] > array[largest])
-        largest = right;
+	if (right < n && array[right] > array[largest])
+		largest = right;
 
-    if (largest != i)
-    {
-        swap(&array[i], &array[largest]);
-        print_array(array, size);
-        heapify(array, size, n, largest);
-    }
+	if (largest != i)
+	{
+		swap(&array[i], &array[largest]);
+		print_array(array, size);
+		heapify(array, size, n, largest);
+	}
 }
 
 /**
@@ -46,16 +47,16 @@ void heapify(int *array, size_t size, size_t n, size_t i)
  */
 void heap_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (int i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, size, i);
+	for (int i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, size, i);
 
-    for (int i = size - 1; i > 0; i--)
-    {
-        swap(&array[0], &array[i]);
-        print_array(array, size);
-        heapify(array, size, i, 0);
-    }
+	for (int i = size - 1; i > 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		print_array(array, size);
+		heapify(array, size, i, 0);
+	}
 }
